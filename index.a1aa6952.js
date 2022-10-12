0d741a512524e9aa32062d8f2bd9dd1c472edbd7
MaksoN03 <105747626+MaksoN03@users.noreply.github.com>
2022-10-12 13:52:33 +0000

    Deploying to gh-pages from @ MaksoN03/team-project@9968239883cc3471b1203762d2d0bee593af02a9 🚀

--- index.a1aa6952.js
+++
@@ -1,2 +0,0 @@
-!function(){var e={openModalBtn:document.querySelector("[data-modal2-open]"),closeModalBtn:document.querySelector("[data-modal2-close]"),modal:document.querySelector("[data-modal2]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)}();
-//# sourceMappingURL=index.a1aa6952.js.map
