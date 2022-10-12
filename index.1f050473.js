13df4cb409478393b3ae26cc80dc9f11993b9bb4
MaksoN03 <105747626+MaksoN03@users.noreply.github.com>
2022-10-12 19:31:15 +0000

    Deploying to gh-pages from @ MaksoN03/team-project@c9f713c8b22da0f745ebfcd96e62fdc07b8f5113 🚀

--- index.1f050473.js
+++
@@ -1,2 +0,0 @@
-new Swiper(".image-slider",{autoHeight:!0,simulateTouch:!0,touchRatio:2,touchAngle:45,grabCursor:!0,slideToClickedSlide:!0,keyboard:{enabled:!0,onlyInViewport:!0,pageUpDown:!0},mousewheel:{sensitivity:1,eventsTarget:"image-slider"},spaceBetween:10,slidesPerGroup:1,centeredSlides:!0,freemode:!0,autoplay:{delay:1e3,stopOnLastSlide:!1,disableOnItneraction:!1},speed:800,effect:"flip",flipEffect:{slideShadows:!0,limitRotation:!0}});
-//# sourceMappingURL=index.1f050473.js.map
